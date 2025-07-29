2dbcb602fc1d0e9edbaf18308f8ef7101d74003f
axiomcat <axiomcat@tuta.com>
2025-07-29 11:46:41 -0500

    Add markdown

---
+++ lua/config/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_browser = 'librewolf'
+    end,
+    ft = { "markdown" },
+  },
+}
